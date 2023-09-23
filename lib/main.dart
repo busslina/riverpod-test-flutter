cbb8d48daea7a0ddf1a90b91d88ed4b63ce4d418
Vicente <busslina@gmail.com>
2023-09-23 12:00:38 +0200

    snapshot

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,32 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'main.g.dart';
+
 // Links
 //
 // - https://docs-v2.riverpod.dev/docs/introduction/getting_started
 
 void main() {
-  runApp(const MainApp());
+  runApp(const ProviderScope(child: MainApp()));
 }
 
-class MainApp extends StatelessWidget {
+@riverpod
+String test(TestRef ref) {
+  return 'Hello world';
+}
+
+class MainApp extends ConsumerWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
+  Widget build(BuildContext context, WidgetRef ref) {
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Text(ref.watch(testProvider)),
         ),
       ),
     );
---
+++ lib/main.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'main.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$testHash() => r'9711891750ffce4b964c3c074241b384a4f519a0';
+
+/// See also [test].
+@ProviderFor(test)
+final testProvider = AutoDisposeProvider<String>.internal(
+  test,
+  name: r'testProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$testHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef TestRef = AutoDisposeProviderRef<String>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
